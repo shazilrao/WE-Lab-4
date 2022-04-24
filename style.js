da56da6d33c23f211115fbb7fa524c9f1cd7f6d0
aqibrehman <rafayabdul0340@gmail.com>
2022-04-24 23:47:55 +0500

    First Commit

---
+++ style.js
@@ -0,0 +1,10 @@
+const operator = prompt('Enter operator ( either +, -, *, / or % ): ');
+var number1 = parseFloat(prompt('Enter first number: '));
+var number2 = parseFloat(prompt('Enter second number: '));
+ let result;
+if (operator == '+') {result = number1 + number2;}
+else if (operator == '-') {result = number1 - number2;}
+else if (operator == '*') {result = number1 * number2;}
+else if (operator == '%') {result = number1 % number2;}
+else {result = number1 / number2;}
+console.log(`${number1} ${operator} ${number2} = ${result}`);
